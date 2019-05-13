a7b808b67af3f7f22551b939138315905dcf888a
evsasse <evsasse@gmail.com>
2019-05-13 13:25:33 -0300

    separate js for mobile menu

---
+++ src/js/components/mobileMenu.js
@@ -0,0 +1,12 @@
+const mobileMenu = () => {
+  jQuery('.js--header__sidebar-button').on('click', () => {
+    jQuery('.main-sidebar').toggleClass('open');
+    jQuery('body').toggleClass('is-menu-open');
+  });
+
+  jQuery('.js--header__side-rules').on('click', () => {
+    jQuery('.main-sidebar').toggleClass('open');
+  });
+};
+
+window.mobileMenu = mobileMenu;
---
+++ src/js/components/openModals.js
@@ -0,0 +1,6 @@
+const openModals = () => {
+  jQuery('.modal[data-show="true"]').modal('show');
+  jQuery('.modal[data-show="true"]').data('show', '');
+};
+
+window.openModals = openModals;
